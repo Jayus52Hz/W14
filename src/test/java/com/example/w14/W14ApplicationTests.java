c2308c0176b80ffaf05c2ee623cae5aae9509d03
Nguyen Thinh <quocthinhmyt@gmail.com>
2024-11-20 09:27:04 +0700

    first commit

---
+++ src/test/java/com/example/w14/W14ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.w14;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class W14ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
